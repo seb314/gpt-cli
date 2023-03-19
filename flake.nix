71978e2e29d378332acadb7cdc84242bc34ce2b2
seb314 <sebastian.bachem@tum.de>
2023-03-19 12:57:10 +0100

    update flake schema & simplify
    It seems somewhat unclear what the correct schema is for nix &
    nix-direnv, and id appears to differ between versions (?).
    The current version is based more or less on
    nix flake new -t github:nix-community/nix-direnv <desired output path> # from https://github.com/nix-community/nix-direnv
    https://serokell.io/blog/practical-nix-flakes
    https://github.com/NixOS/nix/issues/5532
    and
    https://nixos.wiki/wiki/Flakes
    
    With an extra output path alias added because my local nix-direnv
    complains otherwise.

--- flake.nix
+++ flake.nix
@@ -4,65 +4,39 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported (not tested)
-      allSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      # Helper to provide system-specific attributes
-      nameValuePair = name: value: { inherit name value; };
-      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
-      forAllSystems = f: genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-        inherit system;
-      });
-    in
-    {
-      devShells = forAllSystems ({ pkgs, system }: {
-        devShell.${system} =
-          let
-            python = pkgs.python310;
-          in
-          pkgs.mkShell {
-            packages = [
-              (python.withPackages (ps: with ps; [
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python-with-deps = (pkgs.python310.withPackages (ps:
+              with ps; [
                 # ipython
                 # python-lsp-server
                 openai
                 python-dotenv
-              ]))
-            ];
-          };
-      });
-      defaultPackage = forAllSystems ({ pkgs, system }: {
-        ${system} =
-          let
-            python-with-deps = pkgs.python310.withPackages (ps: with ps; [
-                # ipython
-                # python-lsp-server
-                openai
-                python-dotenv
-              ]);
-            in
-              pkgs.stdenv.mkDerivation rec {
-                name = "gpt-cli";
-                src = ./gpt-cli;
-                phases = [ "installPhase" ];
+              ]));
+        devShellDefault = pkgs.mkShell {
+          packages = [ python-with-deps ];
+        };
+        package = pkgs.stdenv.mkDerivation rec {
+          name = "gpt-cli";
+          src = ./gpt-cli;
+          phases = [ "installPhase" ];
 
-                installPhase = ''
-                    runHook preInstall
-                    mkdir -p $out/bin
-                    echo "#!${python-with-deps}/bin/python3" > $out/bin/gpt-cli
-                    cat $src >> $out/bin/gpt-cli
-                    chmod +x $out/bin/gpt-cli
-                    runHook postInstall
-                    '';
-              };
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            echo "#!${python-with-deps}/bin/python3" > $out/bin/gpt-cli
+            cat $src >> $out/bin/gpt-cli
+            chmod +x $out/bin/gpt-cli
+            runHook postInstall
+          '';
+        };
+      in {
+        devShells.default = devShellDefault;
+        devShell = devShellDefault; # add the same under another path because nix-direnv or nix versions seem to expect different paths, or so
+
+        packages.default = package;
+        apps.default = {type = "app"; program = "${package}/bin/gpt-cli";};
       });
-    };
 }
